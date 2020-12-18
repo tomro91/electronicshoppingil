beb315302b0be2c2e7a7c0dfa0cb68472625f648
roy845 <58545785+roy845@users.noreply.github.com>
2020-12-18 16:02:08 +0200

    Update server.js
    fwfew

---
+++ startbootstrap-sb-admin-2-gh-pages/js/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+var nodemailer = require('nodemailer');
+const port = 5500;
+const usersSignup =[];
+
+
+//get example
+app.get("/signup",function(req,res){
+  res.sendFile(__dirname+"/register.html",);
+     });
+
+//get example
+app.get("/login",function(req,res){
+res.sendFile(__dirname+"/login.html",);
+  });
+
+//listening to port 5500
+app.listen(port);
+console.log("listening....");
